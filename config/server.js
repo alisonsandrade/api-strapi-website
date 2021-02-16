c3b571f4eef58090f7bd9f0a4c6df3328c3a67c1
Alison Andrade <alison.sandrade@hotmail.com>
2021-02-16 02:28:09 -0300

    Atualização e configuração Heroku

--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', env.HOST),
   port: env.int('PORT', env.PORT),
-  url: 'https://api-alinepontes.vercel.app/',
+  // url: 'https://api-alinepontes.vercel.app/',
   admin: {
-    url: '/',
-    serveAdminPanel: false,
+    // url: '/',
+    // serveAdminPanel: false,
     auth: {
       secret: env('ADMIN_JWT_SECRET', '5b8e0df791cced22af80c7b234c86805'),
     },
