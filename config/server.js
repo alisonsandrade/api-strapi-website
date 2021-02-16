c4dd3182a3e4fc058693e94aa7165121e6d7d0ef
Alison Andrade <alison.sandrade@hotmail.com>
2021-02-16 01:40:05 -0300

    Refatoraçao da extensao CKEditor

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5b8e0df791cced22af80c7b234c86805'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '34d9a95c-ff9f-4f8f-af37-be258838569a'
+};
