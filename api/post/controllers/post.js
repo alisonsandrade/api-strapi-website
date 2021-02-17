8a570b703b927965149459f2b7ea4988b2daa679
Alison Andrade <alison.sandrade@hotmail.com>
2021-02-17 20:18:05 -0300

    Inclusao do slug como chave primaria para busca no metodo findOne

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -1,8 +1,16 @@
-'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
 
-module.exports = {};
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const entity = await strapi.services.post.findOne({ slug });
+    return sanitizeEntity(entity, { model: strapi.models.post });
+  },
+};
